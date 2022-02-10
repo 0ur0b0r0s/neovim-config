56b5ff18b04ef4e04d2e4fe8cf9ed5dfdfebeb04
0ur0b0r0s <pedro_souza@riseup.net>
2022-02-09 22:05:22 -0300

    set expandtab=2 to sh/lua filetypes

--- lua/settings.lua
+++ lua/settings.lua
@@ -10,7 +10,8 @@ cmd [[
         autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
         autocmd FileType yml setlocal ts=2 sts=2 sw=2 expandtab
         autocmd FileType tf setlocal ts=2 sts=2 sw=2 expandtab
-
+        autocmd FileType lua setlocal ts=2 sts=2 sw=2 expandtab
+        autocmd FileType sh setlocal ts=2 sts=2 sw=2 expandtab
     ]]
 cmd('')
 
