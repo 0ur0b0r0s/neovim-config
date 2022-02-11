9665bb6978637472db435cb34cf850b5f51c43b7
0ur0b0r0s <pedro_souza@riseup.net>
2022-02-10 23:42:09 -0300

    packer added to autocmd; added telescope file_ignore_patterns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,25 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 local use = require('packer').use
-
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   execute 'packadd packer.nvim'
 end
+
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = {'undodir','sessions','packer_compiled'}
+  }
+}
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 return require('packer').startup(function()
     -- Packer can manage itself
