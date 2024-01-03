20d9cf8b6806e261efcc6af2734ef8d457d8efcd
0ur0b0r0s <pedro_souza@riseup.net>
2024-01-03 04:48:31 -0300

    [new-file-structure] - pull request

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,92 +1,131 @@
--- leader mapping
-vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", { noremap = true, silent = true })
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
--- saving
-vim.api.nvim_set_keymap("n", "<c-w><c-q>", ":wq<CR>", { noremap = true, silent=true })
-vim.api.nvim_set_keymap("n", "<c-w><c-w>", ":w!<CR>", { noremap = true, silent=true })
-vim.api.nvim_set_keymap("n", "<c-q><c-q>", ":q!<CR>", { noremap = true, silent=true })
+vim.keymap.set("n", ";", ":", { noremap = true })
+vim.keymap.set("n", "<c-w><c-q>", ":wq<CR>", { noremap = true, silent=true })
+vim.keymap.set("n", "<c-w><c-w>", ":w!<CR>", { noremap = true, silent=true })
+vim.keymap.set("n", "<c-q><c-q>", ":q!<CR>", { noremap = true, silent=true })
 
 -- better window movement
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { silent = true })
+-- vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+-- vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
+-- vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
+-- vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
 
 -- better indenting
-vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
 
 -- I hate escape
-vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+vim.keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
+-- vim.keymap.set("v", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
 
 -- buffers
-vim.api.nvim_set_keymap("n", "<Leader>bn", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>bb", ":bprevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>ba", ":bad ", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>bl", ":ls<CR>", { noremap = true, silent = true })
-
--- wraps
-vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>sw", ":set wrap<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>snw", ":set wrap<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>bn", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>bb", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>ba", ":bad ", { noremap = true })
+vim.keymap.set("n", "<Leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>bl", ":ls<CR>", { noremap = true, silent = true })
 
 -- terminal
-vim.api.nvim_set_keymap("n", "<Leader>tv", ":vne<CR>:terminal<CR>i", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>th", ":sp<CR>:terminal<CR>i", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>tv", ":vne<CR>:terminal<CR>i", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>th", ":sp<CR>:terminal<CR>i", { noremap = true, silent = true })
 
 -- substitute
-vim.api.nvim_set_keymap("n", "S", ":%s//g<Left><Left>", { noremap = true })
+vim.keymap.set("n", "S", ":%s//g<Left><Left>", { noremap = true })
 
 -- git
-vim.api.nvim_set_keymap("n", "<Leader>g", ":Git<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gP", ":Git push<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gp", ":Git pull<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gl", ":Git log<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gv", ":GV<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>g", ":Git<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>gb", ":Telescope git_branches<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>gs", ":Telescope git_status<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>gc", ":Telescope git_commits<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>gnb", ":Git checkout -b ", { noremap = true })
 
 -- signify
-vim.api.nvim_set_keymap("n", "<Leader>st", ":SignifyToggle<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>j", ":call sy#jump#next_hunk(v:count1)<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>k", ":call sy#jump#prev_hunk(v:count1)<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>st", ":SignifyToggle<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>j", ":Gitsigns next_hunk<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>k", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
 
 -- open/source lua config
-vim.api.nvim_set_keymap("n", "<c-e><c-v>", ":tabnew ~/.config/nvim/init.lua<CR>", { noremap = true, silent=true })
-vim.api.nvim_set_keymap("n", "<c-s><c-x>", ":source %<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<c-e><c-v>", ":tabnew ~/.config/nvim/init.lua<CR>", { noremap = true, silent=true })
+vim.keymap.set("n", "<c-s><c-x>", ":source %<CR>", { noremap = true, silent = true })
 
 -- packer
-vim.api.nvim_set_keymap("n", "<Leader>pi", ":PackerInstall<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>pd", ":PackerClean<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ps", ":PackerStatus<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>pi", ":PackerInstall<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>pc", ":PackerClean<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>ps", ":PackerStatus<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>pu", ":PackerUpdate<CR>", { noremap = true })
 
 -- resize windows
-vim.api.nvim_set_keymap("n", "+", "<c-W>+", { noremap = true, silent=true })
-vim.api.nvim_set_keymap("n", "-", "<c-W>-", { noremap = true, silent=true })
+vim.keymap.set("n", "+", "<c-W>+", { noremap = true, silent=true })
+vim.keymap.set("n", "-", "<c-W>-", { noremap = true, silent=true })
 
 -- yank/paste
-vim.api.nvim_set_keymap("v", "<C-y>", "\"+y", { noremap = true, silent=true })
-vim.api.nvim_set_keymap("n", "<c-p><c-p>", "\"+p", { silent=true })
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true, silent=true })
+vim.keymap.set("v", "<C-y>", "\"+y", { noremap = true, silent=true })
+vim.keymap.set("n", "<c-p><c-p>", "\"+p", { silent=true })
+vim.keymap.set("n", "Y", "y$", { noremap = true, silent=true })
 
 -- sessions
-vim.api.nvim_set_keymap("n", "<Leader>sc", ":mksession! ~/.config/nvim/sessions/", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ss", ":bufdo bwipeout | source ~/.config/nvim/sessions/", { noremap = true })
+vim.keymap.set("n", "<Leader>sc", ":mksession! ~/.config/nvim/sessions/", { noremap = true })
+vim.keymap.set("n", "<Leader>ss", ":bufdo bwipeout | source ~/.config/nvim/sessions/", { noremap = true })
 
 -- terraform
-vim.api.nvim_set_keymap("n", "<Leader>ti", ":!source .env | terraform init -no-color<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>tp", ":!terraform plan -no-color -out=last_plan.bin > last_plan.log<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>ta", ":!terraform apply -no-color last_plan.bin<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>op", ":vsplit last_plan.log<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>ti", ":!source .env | terraform init -no-color<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>tp", ":!terraform plan -no-color -out=last_plan.bin > last_plan.log<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>ta", ":!terraform apply -no-color last_plan.bin<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>op", ":vsplit last_plan.log<CR>", { noremap = true })
+
+-- k8s
+vim.keymap.set("n", "<Leader>kad", ":!kubectl apply -f % --dry-run=client<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>ka", ":!kubectl apply -f %<CR>", { noremap = true })
 
 -- docker-compose
-vim.api.nvim_set_keymap("n", "<Leader>db", ":!docker-compose build<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>du", ":!docker-compose up -d<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>dd", ":!docker-compose down<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>dr", ":!docker-compose restart<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>db", ":!docker-compose build<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>du", ":!docker-compose up -d<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>dd", ":!docker-compose down<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>dr", ":!docker-compose restart<CR>", { noremap = true })
+
+-- ChatGPT
+vim.keymap.set("n", "<Leader>cg", ":ChatGPT<CR>", { noremap = true })
+
+-- nvim-dap
+vim.keymap.set('n', '<Leader>dt', [[:lua require"dapui".toggle()<CR>]], { noremap = true })
+vim.keymap.set('n', '<Leader>d1', [[:lua require"dap".toggle_breakpoint()<CR>]], { noremap = true })
+vim.keymap.set('n', '<Leader>d2', [[:lua require"dap".continue()<CR>]], { noremap = true })
+vim.keymap.set('n', '<Leader>d3', [[:lua require"dap".step_over()<CR>]], { noremap = true })
+vim.keymap.set('n', '<Leader>d4', [[:lua require"dap".step_into()<CR>]], { noremap = true })
+-- vim.keymap.set('n', '<Leader>d5', [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true })
+vim.keymap.set('n', '<Leader>ds', [[:lua require"osv".launch({port = 8086})<CR>]], { noremap = true })
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<Leader>mm', ":Mason<CR>", { noremap = true, silent = true })
 
 -- telescope
-vim.api.nvim_set_keymap("n", "<Leader>ff", ":Telescope find_files<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>fb", ":Telescope buffers<CR>", { noremap = true })
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,10 +1,34 @@
-local o = vim.o
-local bo = vim.bo
-local wo = vim.wo
-local cmd = vim.cmd
+--
+-- See `:help vim.o`
+--
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.swapfile = false
+vim.o.backup = false
+vim.o.errorbells = false
+vim.o.expandtab = true
+vim.o.hidden = true
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.shiftwidth = 2
+vim.o.smartcase = true
+vim.o.softtabstop = 0
+vim.o.syntax = 'on'
+vim.o.tabstop = 2
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.updatetime = 250
+vim.o.wrap = false
+vim.o.completeopt = 'menuone,noselect'
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+vim.wo.wrap = false
 
-cmd [[
-        silent! colorscheme palenight
+vim.cmd [[
+        silent! colorscheme tokyonight-night
         autocmd ColorScheme * highlight Normal guibg=NONE ctermbg=NONE
         autocmd ColorScheme * highlight LineNr guibg=NONE guifg=white 
         autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
@@ -12,27 +36,6 @@ cmd [[
         autocmd FileType tf setlocal ts=2 sts=2 sw=2 expandtab
         autocmd FileType lua setlocal ts=2 sts=2 sw=2 expandtab
         autocmd FileType sh setlocal ts=2 sts=2 sw=2 expandtab
+        set laststatus=3
     ]]
-cmd('')
 
-o.termguicolors = true
-o.syntax = 'on'
-o.errorbells = false
-o.smartcase = true
-o.backup = false
-o.undodir = vim.fn.stdpath('config') .. '/undodir'
-o.undofile = true
-o.incsearch = true
-o.hidden = true
-o.tabstop = 4
-o.softtabstop = 0
-o.shiftwidth = 4
-o.expandtab = true
-o.hlsearch = false
-wo.number = true
-wo.relativenumber = true
-wo.signcolumn = 'yes'
-wo.wrap = false
-bo.swapfile = false
-bo.autoindent = true
-bo.smartindent = true
